ee90f364e7c616945a182b62500bb5841a2ee9ae
Zihadul Islam <zihadul10101@gmail.com>
2022-03-30 16:03:38 +0600

    desigan updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react'; import Editor from './Editor';
 function App() {
+  const [html, setHtml] = useState('html', '')
+  const [css, setCss] = useState('css', '')
+  const [js, setJs] = useState('js', '')
   return (
-    <div className="App">
-      <h1>Hello </h1>
-    </div>
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
 

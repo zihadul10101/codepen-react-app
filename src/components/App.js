fe9c93ffe58b995aa5e6202aa66b09a3d1e7ff50
Zihadul Islam <zihadul10101@gmail.com>
2022-03-31 09:56:40 +0600

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
-import React, { useState, useEffect } from 'react'; import Editor from './Editor';
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor';
+import useLocalStorage from '../hooks/useLocalStorage'
 function App() {
-  const [html, setHtml] = useState('html', '')
-  const [css, setCss] = useState('css', '')
-  const [js, setJs] = useState('js', '')
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
   return (
     <>
       <div className="pane top-pane">
@@ -27,7 +45,7 @@ function App() {
       </div>
       <div className="pane">
         <iframe
-
+           srcDoc={srcDoc}
           title="output"
           sandbox="allow-scripts"
           frameBorder="0"
